508bb29b5d9a45877f6bac76c783096861edae15
siddharth-kothari-spk <siddharth.kothari.spk>
2023-06-09 14:36:17 +0530

    added button outlet

--- Camera Sample/ViewController.swift
+++ Camera Sample/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
